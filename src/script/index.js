11c5f77b653c39adb697de3de4e5afa103d1c735
vicDevz <victormrodrguez06@gmail.com>
2023-01-11 17:10:40 -0500

    final style

--- src/script/index.js
+++ src/script/index.js
@@ -1,78 +1,119 @@
 import { videoSources } from "./videoSources.js"
 
+const videoPlayerTemplate = document.createElement('template')
+videoPlayerTemplate.innerHTML = `
+    <img class="contentScrollVideo__img" src="">
+    <div class="listVideos__content--info">
+        <h3></h3>
+        <h4></h4>
+        <h4></h4>
+    </div>
+`
+
+const mainContainer = document.getElementById('main')
+mainContainer.classList.remove('mainContainer')
+const myVideo = document.getElementById("myVideo")
+myVideo.classList.add('inactive')
+const infoVideo = document.getElementById('description-video')
+infoVideo.classList.add('inactive')
+const scrollVideos = document.getElementById('scrollVideo')
+scrollVideos.classList.remove('listVideos')
+
+const seeDescriptionBtn = document.createElement('button')
+seeDescriptionBtn.textContent = 'See Description'
+const ocultDescriptionBtn = document.createElement('button')
+ocultDescriptionBtn.textContent = 'Ocult Description'
+
+seeDescriptionBtn.classList.add('primary-button')
+ocultDescriptionBtn.classList.add('primary-button')
+
+let previouslySelectedItem
 let views = '125.000   :   2 days ago'
-let likes = 80973
 let subscribers = "1.780.954"
 
-const myVideo = document.getElementById("myVideo")
-const infoVideo = document.getElementById('description-video')
-
-const scrollVideos = document.getElementById('scrollVideo')
-
-function videoElementScroll(api) {
-    const allItems = []
-
-    api.forEach((element) => {
-    const thumb = document.createElement('img')
-    thumb.classList.add('contentScrollVideo__img')
-    thumb.src = element.thumb
-
-    const title = document.createElement('h3')
-    title.textContent = element.title
-
-    const author = document.createElement('h4')
-    author.textContent = element.author
-
-    const view = document.createElement('h4')
-    view.textContent = views
-
-    const infoScrollVideo = document.createElement('div')
-    infoScrollVideo.classList.add('listVideos__content--info')
-    infoScrollVideo.append(title, author, view)
+function createVideoElement(video) {
+    const videoElement = videoPlayerTemplate.content.cloneNode(true)
+    videoElement.querySelector('.contentScrollVideo__img').src = video.thumb
+    videoElement.querySelector('h3').textContent = video.title
+    videoElement.querySelector('h4:nth-of-type(1)').textContent = video.author
+    videoElement.querySelector('h4:nth-of-type(2)').textContent = views
 
     const contentScrollVideo = document.createElement('div')
     contentScrollVideo.classList.add('listVideos__content')
-    contentScrollVideo.append(thumb, infoScrollVideo)
+    contentScrollVideo.append(videoElement)
 
     contentScrollVideo.addEventListener('click', () => {
-        let previouslySelectedItem
-        myVideo.src = element.sources
-
+        mainContainer.classList.add('mainContainer')
+        scrollVideos.classList.add('listVideos')
+        myVideo.classList.remove('inactive')
+        infoVideo.classList.remove('inactive')
         infoVideo.innerHTML = ''
-    
+        myVideo.src = video.sources
+
         const authorContent = document.createElement('div')
         authorContent.classList.add('playerContainer__info--author')
+
         const authorInfo = document.createElement('div')
-        authorInfo.classList.add('playerContainer__info--descriiption')
-    
+        authorInfo.classList.add('playerContainer__info--description')
+
         const authorAvatar = document.createElement('img')
-        authorAvatar.src = element.authorAvatar
+        authorAvatar.src = video.authorAvatar
         const authorName = document.createElement('h4')
-        authorName.textContent = element.author
+        authorName.textContent = video.author
         const authorSubscribers = document.createElement('p')
         authorSubscribers.textContent = `${subscribers} subscribers`
-    
-        const title = document.createElement('h3')
-        title.textContent = element.title
-    
-        const description = document.createElement('p')
-        description.textContent = element.description
-    
+
         authorInfo.append(authorName, authorSubscribers)
         authorContent.append(authorAvatar, authorInfo)
-        infoVideo.append(authorContent, title, description)
+        infoVideo.append(authorContent)
 
         if (previouslySelectedItem) {
             scrollVideos.insertBefore(previouslySelectedItem, scrollVideos.firstChild)
-          }
-          previouslySelectedItem = contentScrollVideo
+        }
+
+        previouslySelectedItem = contentScrollVideo
 
         scrollVideos.removeChild(contentScrollVideo)
+
+        handleSeeDescriptionBtn(video)
     })
-    
-    allItems.push(contentScrollVideo)
-    })
-    scrollVideos.append(...allItems)
+
+    return contentScrollVideo
 }
 
-videoElementScroll(videoSources)
+function handleSeeDescriptionBtn(video) {
+    infoVideo.append(seeDescriptionBtn)
+    seeDescriptionBtn.classList.remove('inactive')
+
+    const title = document.createElement('h3')
+    title.textContent = video.title
+
+    const description = document.createElement('p')
+    description.textContent = video.description
+
+    title.classList.add('inactive')
+    description.classList.add('inactive')
+    ocultDescriptionBtn.classList.add('inactive')
+
+    infoVideo.append(title, description, ocultDescriptionBtn)
+
+    seeDescriptionBtn.addEventListener('click', () => {
+        seeDescriptionBtn.classList.add('inactive')
+
+        title.classList.remove('inactive')
+        description.classList.remove('inactive')
+        ocultDescriptionBtn.classList.remove('inactive')
+
+        ocultDescriptionBtn.addEventListener('click',()=>handleOcultDescriptionBtn(title,description))
+    })
+}
+
+function handleOcultDescriptionBtn(title, description) {
+    title.classList.add('inactive')
+    description.classList.add('inactive')
+    ocultDescriptionBtn.classList.add('inactive')
+
+    seeDescriptionBtn.classList.remove('inactive')
+}
+
+videoSources.forEach(itemVideo => scrollVideos.appendChild(createVideoElement(itemVideo)));
